36cb55328eb1b56a0920fe7d3bfc8272f68d9911
Maksymilian Wojczuk <max.wojczuk@appliscale.io>
2018-04-10 10:55:12 +0200

    initial commit

---
+++ src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/VmmsBackendApplication.java
@@ -0,0 +1,12 @@
+package pl.agh.edu.iisg.io.vmms.vmmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmmsBackendApplication.class, args);
+	}
+}
