ef4dcf485ce2a1b6f1137d913f2314f8b5ba511a
michaelloo35 <michaelloo35@gmail.com>
2018-04-16 23:31:38 +0200

    Login fixed

--- src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/model/ApplicationUserDetailsServiceImpl.java
+++ src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/model/ApplicationUserDetailsServiceImpl.java
@@ -4,10 +4,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import pl.agh.edu.iisg.io.vmms.vmmsbackend.repository.UserRepository;
 
 import static java.util.Collections.emptyList;
 
+@Service
 public class ApplicationUserDetailsServiceImpl implements UserDetailsService {
 
     private UserRepository userRepository;
--- src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/security/JWTAuthenticationFilter.java
+++ src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/security/JWTAuthenticationFilter.java
@@ -59,4 +59,9 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .compact();
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
     }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
 }
