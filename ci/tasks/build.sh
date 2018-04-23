f63c93d27d638f963d760b15e015844113e2356c
fapfood <fapfood@gmail.com>
2018-04-23 21:47:16 +0200

    check

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,7 +6,7 @@ cd sourcecode
 #
 #export DOCKER_HOST=127.0.0.1:54321
 
-dockerd --iptables=false & docker-compose up -d
+docker run -v /var/run/docker.sock:/var/run/docker.sock & docker-compose up -d
 
 ./gradlew clean build
 
