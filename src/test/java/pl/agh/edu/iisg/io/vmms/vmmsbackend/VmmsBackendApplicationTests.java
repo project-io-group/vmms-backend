844cbe79f9ef5ac540b3aee157708879fbe4b02b
fapfood <fapfood@gmail.com>
2018-06-06 23:36:09 +0200

    add sendgrid var

--- src/test/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/VmmsBackendApplicationTests.java
+++ src/test/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/VmmsBackendApplicationTests.java
@@ -3,9 +3,11 @@ package pl.agh.edu.iisg.io.vmms.vmmsbackend;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("dev")
 @SpringBootTest
 public class VmmsBackendApplicationTests {
 
