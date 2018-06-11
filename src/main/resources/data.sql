8fe800dfa832f794fe010d3d219153c6a14c969d
michaelloo35 <michaelloo35@gmail.com>
2018-06-12 00:48:33 +0200

    Bye gringo bye gonzo

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (id, password, email) VALUES
-  (nextval('hibernate_sequence'), '123', 'vmms@vmms.com'),
-  (nextval('hibernate_sequence'), '123', 'admin@vmms.com'),
-  (nextval('hibernate_sequence'), '123', 'gonzo@vmms.com'),
-  (nextval('hibernate_sequence'), '123', 'gringo@vmms.com');
