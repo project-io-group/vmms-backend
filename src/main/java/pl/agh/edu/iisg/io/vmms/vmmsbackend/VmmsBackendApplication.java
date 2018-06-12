de3b574a14dba587295b6d15cfcacf84f9ba2bd3
fapfood <fapfood@gmail.com>
2018-06-12 10:07:13 +0200

    utc fix and cleanup imports

--- src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/VmmsBackendApplication.java
+++ src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/VmmsBackendApplication.java
@@ -8,13 +8,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import pl.agh.edu.iisg.io.vmms.vmmsbackend.task.ReservationCleanupTask;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
 public class VmmsBackendApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+        System.out.println("Spring boot application running in CET timezone :" + new Date());
+    }
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(VmmsBackendApplication.class);
--- src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/service/VMPoolService.java
+++ src/main/java/pl/agh/edu/iisg/io/vmms/vmmsbackend/service/VMPoolService.java
@@ -1,16 +1,10 @@
 package pl.agh.edu.iisg.io.vmms.vmmsbackend.service;
 
 import org.springframework.web.multipart.MultipartFile;
-import pl.agh.edu.iisg.io.vmms.vmmsbackend.controller.VMPoolController;
 import pl.agh.edu.iisg.io.vmms.vmmsbackend.exception.VMPoolImportFileException;
 import pl.agh.edu.iisg.io.vmms.vmmsbackend.model.VMPool;
-import pl.agh.edu.iisg.io.vmms.vmmsbackend.parsers.VMPoolCSVParser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Optional;
 
 public interface VMPoolService {
     List<VMPool> getVMPools();
